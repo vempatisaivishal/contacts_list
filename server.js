fd66adcb77df7edb64818d6f1282c0fd305b804d
sai vishal vempati <106369240+vempatisaivishal@users.noreply.github.com>
2025-01-19 23:10:33 +0530

    version 2 has been added with jwt
    This version has implementation of the jwt

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.get("/api/contacts",(req,res)=>{
+    res.status(200).json({message:"Get all contacts"});
+});
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
